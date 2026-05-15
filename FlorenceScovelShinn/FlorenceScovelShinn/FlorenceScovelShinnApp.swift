c5993ed39d641ca4458048f95dbb873e43a4d5cb
Amund Ring <amund.ring@schibsted.com>
2026-05-15 15:17:18 +0200

    create project

---
+++ FlorenceScovelShinn/FlorenceScovelShinn/FlorenceScovelShinnApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlorenceScovelShinnApp.swift
+//  FlorenceScovelShinn
+//
+//  Created by Amund Ring on 15/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlorenceScovelShinnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
