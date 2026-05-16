955191bcd0968ad44fafd8def23ee63387bda08f
Amund Ring <amund.ring@schibsted.com>
2026-05-16 21:44:59 +0200

    Update PressableCardStyle.swift

--- FlorenceScovelShinn/Theme/PressableCardStyle.swift
+++ FlorenceScovelShinn/Theme/PressableCardStyle.swift
@@ -3,7 +3,7 @@ import SwiftUI
 /// A button style that gives the wrapped view a subtle scale-down on press,
 /// creating tactile feedback for tappable cards and rows.
 struct PressableCardStyle: ButtonStyle {
-    var pressedScale: CGFloat = 0.97
+    var pressedScale: CGFloat = 0.98
 
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
